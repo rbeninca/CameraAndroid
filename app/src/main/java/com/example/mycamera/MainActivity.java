baf383852afd2b3fcf9e9d6ee2fc22e0e5f28112
rbeninca <rbeninca@gmail.com>
2023-11-23 15:48:17 -0300

    solicitação de permissões

--- app/src/main/java/com/example/mycamera/MainActivity.java
+++ app/src/main/java/com/example/mycamera/MainActivity.java
@@ -1,13 +1,35 @@
 package com.example.mycamera;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.Manifest;
 
 public class MainActivity extends AppCompatActivity {
-
+    String [] permissions= new String [] {
+            Manifest.permission.CAMERA,
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.RECORD_AUDIO,
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //solicitar e checagem de permissão camera e armazenamento
+        checkPermissoes();
+
     }
+    public  void  checkPermissoes(){
+        //Checando as permissões foram concedidas ou as solicitando ao usuário
+        for (String permission: permissions) {
+            if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED)
+            {
+                requestPermissions(permissions,PackageManager.PERMISSION_GRANTED);
+            }
+        }
+    }
+
 }
